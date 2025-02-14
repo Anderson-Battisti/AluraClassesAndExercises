f864969d07f051d2c4b6ad758e5f566f284db333
Anderson Battisti <anderson.battisti@universo.univates.br>
2025-02-13 22:38:30 -0300

    Java OOP: Aula 1 criando classes, instanciando objetos e chamando métodos

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main
 {
     public static void main( String[] args )
     {
-        double originalPrice = 50.45;
-        double discountPercent = 10;
-        double discountPrice = 0;
+        Person person = new Person();
+        person.printMessage();
 
-        discountPrice -= ( originalPrice * discountPercent ) / 100;
-
-        System.out.println( discountPrice );
+        Student student = new Student();
+        student.setName( "John" );
+        student.setAge( 22 );
+        student.showStudentInformation();
     }
 }
